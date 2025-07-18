81405b912b530fb4eac7218ae247f723596a932d
Vytautas Luksys <vytautas.luksys@cgi.com>
2025-07-18 15:21:40 +0300

    chore: Vue components package version bump

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
+import i18next from 'i18next'
+import I18NextVue from 'i18next-vue'
 import vuetify from './vuetify'
 import router from './router'
 import rcSesVueComponents from './rc-ses-vue-components'
 
 function registerPlugins(app: App) {
-  app.use(vuetify).use(rcSesVueComponents).use(router)
+  app.use(I18NextVue, { i18next }).use(vuetify).use(rcSesVueComponents).use(router)
 }
 
 export default registerPlugins
